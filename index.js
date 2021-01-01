7e8f55b34b3bb2472d4c6a5518ea77f6c4dd58d7
genadi60 <genadimihaylov@gmail.com>
2021-01-01 03:27:13 +0200

    create cube model and templates

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
-const app = require('express')();
+const express = require('express');
+const app = express();
+const home = require('./routes/home');
 
 require('./config/express')(app);
-require('./config/routes')(app);
+//require('./config/routes')(app);
+
+app.use('/', home);
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
