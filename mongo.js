5e1a834981ebf45f08d9ecc2d710ce3986ee08b0
genadi60 <genadimihaylov@gmail.com>
2021-01-03 23:33:48 +0200

    Extended to work with MongoDB

--- mongo.js
+++ mongo.js
@@ -3,25 +3,25 @@
 // const connectionStr = 'mongodb://localhost:27017';
 // const client = new MongoClient(connectionStr, {useUnifiedTopology: true});
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const DB_USER = process.env.DB_USER || '';
-const DB_PASS = process.env.DB_PASS || '';
-const DB_NAME = process.env.DB_NAME || '';
-const connectionStr1 = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.ih30w.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-const connectionStr = 'mongodb://localhost:27017/test';
-mongoose.connect(connectionStr);
+// const DB_USER = process.env.DB_USER || '';
+// const DB_PASS = process.env.DB_PASS || '';
+// const DB_NAME = process.env.DB_NAME || '';
+// const connectionStr1 = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.ih30w.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+// const connectionStr = 'mongodb://localhost:27017/test';
+// mongoose.connect(connectionStr);
 
-const dbQueries = async (collection) => {
-  try {
-    //await collection.insertOne({ 'name': 'Pesho' });
-    const data = await collection.find().toArray();
-    console.log('Async result: ', data);
-  } catch (error) {
-    console.error(error.message);
-    throw error;
-  }
-}
+// const dbQueries = async (collection) => {
+//   try {
+//     //await collection.insertOne({ 'name': 'Pesho' });
+//     const data = await collection.find().toArray();
+//     console.log('Async result: ', data);
+//   } catch (error) {
+//     console.error(error.message);
+//     throw error;
+//   }
+// }
 
 // client.connect(function(err) {
 //   if (err) {
